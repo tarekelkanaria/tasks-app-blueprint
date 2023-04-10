361584155e7d8786c2cd06537d52ed5cec012b1f
Tarek Elkanaria <tarekelkanaria@gmail.com>
2023-04-10 17:23:14 +0200

    Modify Validation

--- src/components/AddTask/AddTask.jsx
+++ src/components/AddTask/AddTask.jsx
@@ -1,54 +1,52 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 import useRequest from "../../hooks/use-request";
 import classes from "./AddTask.module.css";
 
 const AddTask = ({ onAdd }) => {
   const { isLoading, isError, errorText, sendRequest } = useRequest();
-  const taskText = useRef(null);
-  const [isValid, setIsValid] = useState(true);
-  const [isWriting, setIsWriting] = useState(false);
 
-  const handleWriting = () => {
-    setIsValid(true);
-    setIsWriting(true);
+  const [taskText, setTaskText] = useState("");
+  const [isTouched, setIsTouched] = useState(false);
+
+  const validText = taskText.trim().length > 3;
+  const invalidInput = !validText && isTouched;
+
+  const handleEnteredText = (e) => {
+    setTaskText(e.target.value);
+    setIsTouched(false);
   };
 
-  let enteredValue = "";
-
-  const handleEnteredText = () => {
-    enteredValue = taskText.current.value;
-  };
-
-  const handleValidation = () => {
-    if (enteredValue.trim().length < 3) setIsValid(false);
-    setIsWriting(false);
+  const handleBlurValidation = () => {
+    setIsTouched(true);
   };
 
   const extractData = (data) => {
     const generatedId = data.name;
-    const createdTask = { id: generatedId, text: enteredValue };
+    const createdTask = { id: generatedId, text: taskText };
     onAdd(createdTask);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    enteredValue = taskText.current.value;
-    if (isValid && enteredValue.length) {
-      sendRequest(
-        {
-          method: "post",
-          url: "https://favorite-movies-2ce9f-default-rtdb.firebaseio.com/tasks.json",
-          task: enteredValue,
-          headers: {
-            "Content-Type": "application/json",
-          },
-        },
-        extractData
-      );
-    } else {
-      setIsValid(false);
+    setIsTouched(true);
+
+    if (!validText) {
+      return;
     }
-    taskText.current.value = "";
+
+    sendRequest(
+      {
+        method: "post",
+        url: "https://favorite-movies-2ce9f-default-rtdb.firebaseio.com/tasks.json",
+        task: taskText,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+      extractData
+    );
+    setIsTouched(false);
+    setTaskText("");
   };
 
   return (
@@ -57,20 +55,19 @@ const AddTask = ({ onAdd }) => {
         <div className={classes["user_action"]}>
           <div
             className={`${classes["user_input"]} ${
-              isWriting && classes.writing
+              invalidInput && classes.writing
             }`}
           >
             <input
               type="text"
-              ref={taskText}
               placeholder="Enter the task here"
+              value={taskText}
               onChange={handleEnteredText}
-              onBlur={handleValidation}
-              onFocus={handleWriting}
+              onBlur={handleBlurValidation}
             />
-            {!isValid && (
+            {invalidInput && (
               <p className={classes["valid-message"]}>
-                This field is required with more than two characters!
+                This field is required with more than three characters!
               </p>
             )}
           </div>
